91d8172e799f99db6935668c2225a1c214d862a7
navin <navinbhaskariitkgp@gmail.com>
2018-07-18 19:25:52 +0530

    testing

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from webapp import views
 
 
 urlpatterns = [
-    # url(r'^allsubmissions/(?P<user_id>[A-Za-z0-9_.-]+)/$', views.AllTestSubmissions.as_view()),
     url(r'^streak/(?P<student_id>[A-Za-z0-9_.-]+)/$', views.streak.as_view(), name='streak'),
 ]
